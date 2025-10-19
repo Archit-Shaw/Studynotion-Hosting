17ea9748d9129e3d5274ca199893dc45518c0c9d
ArchitShaw <148678912+Archit-Shaw@users.noreply.github.com>
2025-10-20 02:11:29 +0530

    Refactor mailSender for improved error logging

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,22 +2,26 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
-    
+
+    // 1. Configure the transporter
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST, 
+      host: process.env.MAIL_HOST,
       port: 465, 
       secure: true, 
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+   
+    });
 
-    // await transporter.verify();
-    // console.log("Transporter ready for sending.");
+   
+    await transporter.verify();
+    console.log("Transporter ready for sending.");
 
-    // Send the email
+    
     let info = await transporter.sendMail({
-      from: `"StudyNotion ||  - by Archit" <${process.env.MAIL_USER}>`,
+      from: `"StudyNotion || - by Archit" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
@@ -26,7 +30,8 @@ const mailSender = async (email, title, body) => {
     console.log("Email sent successfully:", info.messageId);
     return info;
   } catch (error) {
-    console.error("Error while sending mail:", error.message);
+   
+    console.error("Error while sending mail:", error);
     throw error;
   }
 };
